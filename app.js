4db0026a906880626c616bd442a72d292e17453b
Nicole Tilbe <nicolecali2406@outlook.com>
2020-07-31 12:59:35 -0400

    trying to connect API

---
+++ app.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', ()=>{  /* This function (and everything inside this function) will run when page loads */
+    let long;
+    let lat;
+
+    if(navigator.geolocation){ /* Will only work if user allows window to find location, click "allow" when page loads */
+        navigator.geolocation.getCurrentPosition(position =>{
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            /*console.log(position); This to check latitude/longitude in console*/
+            const proxy = `http://cors-anywhere.herokuapp.com/`;
+            const api= `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=99db2c78f1a37b58406fe0247c0cc1a6`; 
+
+            fetch(api)
+                .then(response =>{
+                    return response.json();
+                })
+                .then(data =>{
+                    console.log(data);
+                }); 
+        });
+    }
+});
